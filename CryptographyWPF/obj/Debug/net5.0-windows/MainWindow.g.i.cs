c2a171a39868fa64d09fcb0422880f20e450770c
germin701 <P20012293@student.newinti.edu.my>
2024-01-17 04:02:16 +0800

    Remove Unused Package

--- CryptographyWPF/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ CryptographyWPF/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -237,7 +237,7 @@ namespace CryptographyWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CryptographyWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CryptographyWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
